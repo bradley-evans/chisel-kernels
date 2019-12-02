62363e21e4345f17b49478fd1271d46945d9deb0
Bradley Evans <brad@artificer.bblv>
2019-12-02 00:27:58 -0800

    VecMul tests still broken, adding VecAvg

--- src/test/scala/test_VecMul.scala
+++ src/test/scala/test_VecMul.scala
@@ -10,21 +10,26 @@ class VecMulTests(c: VecMul) extends PeekPokeTester(c) {
   val matB = Array.fill(c.n * c.p) {
     rnd.nextInt(1 << c.w)
   }
-  var sum = Int 
+  var sum = 0
   // Poke cannot use vectors directly right now,
   // so we poke each element individually.
   matA.zipWithIndex.foreach { case (value, index) => poke(c.io.A(index), value.U) }
   matB.zipWithIndex.foreach { case (value, index) => poke(c.io.B(index), value.U) }
   poke(c.io.load, true.B)
   step(1)
+  var done = peek(c.io.valid)
+  while (done != 1) {
+    step(1)
+    done = peek(c.io.valid)
+  }
   // Now we can check the output matrix on c.io.C.
   for (i <- 0 until c.m) {
     for (j <- 0 until c.p) {
-      sum = 0.S
+      sum = 0
       for (k <- 0 until c.n) {
         sum = sum + matA(i*c.n+k) * matB(k*c.p+j)
       }
-      expect(c.io.C(i*c.p+j),sum)
+      expect(c.io.C(i*c.p+j),sum.U)
     }
   }
 }
